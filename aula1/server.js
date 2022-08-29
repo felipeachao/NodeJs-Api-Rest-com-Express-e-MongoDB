496a0223530199dc5318447dceefb5b4cb2e994a
joao.achao <joao.achao@conecthus.org.br>
2022-08-29 17:11:11 -0400

    feat: add routes

--- aula1/server.js
+++ aula1/server.js
@@ -1,9 +1,16 @@
 const http = require('http')
 const port = 3000
 
+const routes = {
+    '/': 'Curso de Node',
+    '/livros': 'Entrei na página de livros',
+    '/autores': 'Listagem de autores',
+    '/sobre': 'Info sobre o projeto'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Curso de Node')
+    res.end(routes[req.url])
 })
 
 server.listen(port, () => {
